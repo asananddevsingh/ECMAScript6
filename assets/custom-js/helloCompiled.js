46aa889dc2fa67bb070ddd4a98e83971b59c640f
Anand Dev Singh <ananddevsingh.aiet@gmail.com>
2016-05-29 02:15:40 +0530

    First program using ES6 - Hello world

---
+++ assets/custom-js/helloCompiled.js
@@ -0,0 +1,7 @@
+$traceurRuntime.registerModule("hello.js", [], function() {
+  "use strict";
+  var __moduleName = "hello.js";
+  console.log('Hello world');
+  return {};
+});
+$traceurRuntime.getModule("hello.js" + '');
