69716e96ae32ef26f209e8264fd94de8a05379b6
Anand Dev Singh <ananddevsingh.aiet@gmail.com>
2016-05-29 02:00:37 +0530

    Setting up nodejs and traceur and compiling the code

---
+++ assets/custom-js/1Compiled.js
@@ -0,0 +1,7 @@
+$traceurRuntime.registerModule("1.js", [], function() {
+  "use strict";
+  var __moduleName = "1.js";
+  console.log('This is first program using traceur');
+  return {};
+});
+$traceurRuntime.getModule("1.js" + '');
